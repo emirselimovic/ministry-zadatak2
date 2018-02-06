c269cbad1e162c9353fc2f6fa50acc514c3d9281
ermin <ermin10@MacBook-Pro-od-ermin.local>
2018-02-06 23:54:09 +0100

    Add existing file

--- README.md
+++
@@ -1,1 +0,0 @@
-# ministry-zadatak2
--- emir.sql
+++
@@ -1,6 +0,0 @@
-CREATE TRIGGER audit_ins AFTER delete 
-        ON tblusers FOR each ROW
-        EXECUTE PROCEDURE events();
-		
-		
-		select *  from tbleventsqueue
